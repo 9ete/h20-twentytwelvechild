f98e92cad4477e5af5519b81fe771f40308c47f3
Pete Lower <pete.lower@gmail.com>
2014-03-03 17:31:26 -0600

    Add initial files

---
+++ home.php
@@ -0,0 +1,42 @@
+<?php
+/*
+Template Name: Landing Page
+
+*
+* @package WordPress
+* @subpackage Twenty_Twelve
+* @since Twenty Twelve 1.0
+*/
+
+get_header(); ?>
+
+	<div id="primary" class="site-content">
+		<div id="content" role="main">
+			<section id='slider-section' class='content-section slider-section' >
+				<h2>Live </span>Waterproof</span></h2>
+			</section>
+			<section id='device-section' class='content-section device-section' >
+				<h2>It's a </span>Lifestyle!</span></h2>
+
+				<p>
+					H20dry Smart Phone and iPad Cases allow you to maintain your busy lifestyle while working, having fun or
+
+					BOTH! H2Odry cases protect your device from dust, dirt and water and allow full functionality while using 
+
+					your device at any number of outdoor activities including at the beach, pool, fishing, skiing, kayaking, raft-
+					ing, snow skiing or even in a relaxing bath! We offer serval different sizes of cell phone cases to fit all smart 
+
+					phones currently on the market.
+				</p>
+			</section>
+			
+
+		</div><!-- #content -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
+
+<?php /* while ( have_posts() ) : the_post(); ?>
+				<?php get_template_part( 'content', 'page' ); ?>
+				<?php comments_template( '', true ); ?>
+			<?php endwhile; // end of the loop. */?>
