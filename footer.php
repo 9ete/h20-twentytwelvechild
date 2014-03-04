e90a61b9d68c2cceea635f6517f8378306e53d15
Pete Lower <pete.lower@gmail.com>
2014-03-04 11:22:24 -0600

    Add footer divs for structure

--- footer.php
+++ footer.php
@@ -11,17 +11,25 @@
 ?>
 	</div><!-- #main .wrapper -->
 	<footer id="colophon" role="contentinfo">
-
-		<nav id="footer-navigation" class="footer-navigation" role="navigation">
-			<h3 class="menu-toggle"><?php _e( 'Menu', 'twentytwelve' ); ?></h3>
-			<a class="assistive-text" href="#content" title="<?php esc_attr_e( 'Skip to content', 'twentytwelve' ); ?>"><?php _e( 'Skip to content', 'twentytwelve' ); ?></a>
-			<?php wp_nav_menu( array( 'theme_location' => 'footer-menu', 'menu_class' => 'footer-menu' ) ); ?>
-		</nav><!-- #site-navigation -->
+		<div id='footer-left' class='footer-left'>
+			<img src='/wp-content/uploads/2014/03/h2-dry-iphone-waterproof-case.png' />
+		</div>
+		<div id='footer-right' class='footer-right'>
+			<nav id="footer-navigation" class="footer-navigation" role="navigation">
+				<h3 class="menu-toggle"><?php _e( 'Menu', 'twentytwelve' ); ?></h3>
+				<a class="assistive-text" href="#content" title="<?php esc_attr_e( 'Skip to content', 'twentytwelve' ); ?>"><?php _e( 'Skip to content', 'twentytwelve' ); ?></a>
+				<?php wp_nav_menu( array( 'theme_location' => 'footer-menu', 'menu_class' => 'footer-menu' ) ); ?>
+			</nav><!-- #site-navigation -->
+		</div>
 
 		<div class="site-info">
 			<?php do_action( 'twentytwelve_credits' ); ?>
-			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'twentytwelve' ) ); ?>" title="<?php esc_attr_e( 'Semantic Personal Publishing Platform', 'twentytwelve' ); ?>"><?php printf( __( 'Proudly powered by %s', 'twentytwelve' ), 'WordPress' ); ?></a>
+			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'twentytwelve' ) ); ?>" title="<?php esc_attr_e( 'Semantic Personal Publishing Platform', 'twentytwelve' ); ?>">
+				<?php printf( __( 'Proudly powered by %s', 'twentytwelve' ), 'WordPress' ); ?>
+			</a>
 		</div><!-- .site-info -->
+		
+		
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
