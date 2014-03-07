c990c70b82c5aa1f7252e0ee29ce637a6730f651
root <root@s15809313.onlinehome-server.com>
2014-03-07 10:58:42 -0600

    restructure footer html

--- footer.php
+++ footer.php
@@ -10,28 +10,28 @@
  */
 ?>
 	</div><!-- #main .wrapper -->
-	<footer id="colophon" role="contentinfo">
-		<div id='footer-left' class='footer-left'>
-			<img src='/wp-content/themes/h20-twentytwelvechild/h20-dry-iphone-waterproof-case.png' />
-		</div>
-		<div id='footer-right' class='footer-right'>
-			<nav id="footer-navigation" class="footer-navigation" role="navigation">
-				<h3 class="menu-toggle"><?php _e( 'Menu', 'twentytwelve' ); ?></h3>
-				<a class="assistive-text" href="#content" title="<?php esc_attr_e( 'Skip to content', 'twentytwelve' ); ?>"><?php _e( 'Skip to content', 'twentytwelve' ); ?></a>
-				<?php wp_nav_menu( array( 'theme_location' => 'footer-menu', 'menu_class' => 'footer-menu' ) ); ?>
-			</nav><!-- #site-navigation -->
-		</div>
-
-		<div class="site-info">
-			<p>Copyright © <?php echo date('Y'); ?>, H2Odry.com, All Rights Reserved</p>
-		
-			<?php //do_action( 'twentytwelve_credits' ); ?>
-			<a href="<?php echo esc_url( __( 'http://lowermedia.net/', 'lowermedia' ) ); ?>" title="<?php esc_attr_e( 'Primo Web Applications', 'lowermedia' ); ?>">
-				<?php printf( __( 'Website Designed and Developed by Kelsey Huebner in Association with %s', 'lowermedia' ), 'LowerMedia' ); ?>
-			</a>
-		</div><!-- .site-info -->
-		
-		
+	<footer id="colophon" role="contentinfo" class="footer colophon">
+		<div class="page-middle">
+			<div id='footer-left' class='footer-left'>
+				<img src='/wp-content/themes/h20-twentytwelvechild/h20-dry-iphone-waterproof-case.png' />
+			</div>
+			<div id='footer-right' class='footer-right'>
+				<nav id="footer-navigation" class="footer-navigation" role="navigation">
+					<h3 class="menu-toggle"><?php _e( 'Menu', 'twentytwelve' ); ?></h3>
+					<a class="assistive-text" href="#content" title="<?php esc_attr_e( 'Skip to content', 'twentytwelve' ); ?>"><?php _e( 'Skip to content', 'twentytwelve' ); ?></a>
+					<?php wp_nav_menu( array( 'theme_location' => 'footer-menu', 'menu_class' => 'footer-menu' ) ); ?>
+				</nav><!-- #site-navigation -->
+			</div>
+	
+			<div class="site-info">
+				<p>Copyright © <?php echo date('Y'); ?>, H2Odry.com, All Rights Reserved</p>
+			
+				<?php //do_action( 'twentytwelve_credits' ); ?>
+				<a href="<?php echo esc_url( __( 'http://lowermedia.net/', 'lowermedia' ) ); ?>" title="<?php esc_attr_e( 'Primo Web Applications', 'lowermedia' ); ?>">
+					<?php printf( __( 'Website Designed and Developed by Kelsey Huebner in Association with %s', 'lowermedia' ), 'LowerMedia' ); ?>
+				</a>
+			</div><!-- .site-info -->
+		</div><!-- .page-middle -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
