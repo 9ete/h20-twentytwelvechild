e42dafb19dec8440a8dd2c4ee8200aecac0917f9
root <root@s15809313.onlinehome-server.com>
2014-03-10 16:07:14 -0500

    adding custom jquery

--- lowermedia_js/device-nav.js
+++ lowermedia_js/device-nav.js
@@ -1,17 +1,65 @@
 jQuery(function() {
-    jQuery( "#first-link-wrap" ).hover(function() {
-      jQuery( "#first-img-wrap" ).toggleClass( "show-device", 1000 );
-      return false;
-    });
+    jQuery( "#first-link-wrap" ).hover(
+      function() {
+        jQuery( "#first-img-wrap" ).animate({
+          top:"30px"
+        }, 500);
+        jQuery( "#first-text-wrap" ).css({
+          display:"block"
+        }, 500);
+        return false;
+      },
+      function () {
+        jQuery( "#first-img-wrap" ).animate({
+          top:"140px"
+        }, 500);
+        jQuery( "#first-text-wrap" ).css({
+          display:"none"
+        }, 500);
+        return false;
+      }
+    );
 
-    jQuery( "#second-link-wrap" ).hover(function() {
-      jQuery( "#second-img-wrap" ).toggleClass( "show-device", 1000 );
-      return false;
-    });
+    jQuery( "#second-link-wrap" ).hover(
+      function() {
+        jQuery( "#second-img-wrap" ).animate({
+          top:"30px"
+        }, 500);
+        jQuery( "#second-text-wrap" ).css({
+          display:"block"
+        }, 500);
+        return false;
+      },
+      function () {
+        jQuery( "#second-img-wrap" ).animate({
+          top:"140px"
+        }, 500);
+        jQuery( "#second-text-wrap" ).css({
+          display:"none"
+        }, 500);
+        return false;
+      }
+    );
 
-    jQuery( "#third-link-wrap" ).hover(function() {
-      jQuery( "#third-img-wrap" ).toggleClass( "show-device", 1000 );
-      return false;
-    });
+    jQuery( "#third-link-wrap" ).hover(
+      function() {
+        jQuery( "#third-img-wrap" ).animate({
+          top:"30px"
+        }, 500);
+        jQuery( "#third-text-wrap" ).css({
+          display:"block"
+        }, 500);
+        return false;
+      },
+      function () {
+        jQuery( "#third-img-wrap" ).animate({
+          top:"140px"
+        }, 500);
+        jQuery( "#third-text-wrap" ).css({
+          display:"none"
+        }, 500);
+        return false;
+      }
+    );
 
 });
